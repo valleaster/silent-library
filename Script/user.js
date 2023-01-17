b31c4d73f23ccebfd52e3a519c3a938615264f1d
valleaster <vallirieeasterjohnson@gmail.com>
2023-01-17 10:51:37 +0800

    Update user

--- Script/user.js
+++ Script/user.js
@@ -1,80 +1,75 @@
-const form = document.querySelector('form');
-const formName = document.getElementById('form-name');
-const userEmail = document.getElementById('form-email');
-const userRole = document.getElementById('form-role');
-const fields = [formName, userEmail, userRole];
+const errorMessage = document.getElementById('error-message');
+const addBtn = document.getElementById('add');
+const updateBtn = document.getElementById('update');
+
+const usersName = document.getElementById('name');
+const usersEmail = document.getElementById('email');
+const usersRole = document.getElementById('role');
+const fields = [usersName, usersEmail, usersRole];
 
 let hasError = false;
-let editMode = false;
 let users = JSON.parse(localStorage.getItem('users')) || [];
 
-form.addEventListener('submit', e => {
+// Form submission behavior
+addBtn.addEventListener('click', e => {
     hasError = false;
     e.preventDefault();
     validateForm();
 
-    const username = formName.value.toLowerCase();
-    const email = userEmail.value.toLowerCase();
-    const role = userRole.value;
+    const username = usersName.value.toLowerCase();
+    const email = usersEmail.value.toLowerCase();
+    const role = usersRole.value;
 
-    // Checking for duplicate users
-    const existingUser = users.some(user => user.username === username && user.email === email || user.email === email);
+    // Checking for user existence 
+    const existingUser = users.some(user => user.email === email);
 
     if(existingUser) {
-        showError(formName, 'User already exists');
-        showError(userEmail, 'User already exists');
-    } else if(!hasError) {
-        users.push({ username, email, role });
-        localStorage.setItem('users', JSON.stringify(users));
-    
-        addUser(username, email, role); 
-    }
+        showError(usersEmail);
+        errorMessage.textContent = 'Email already exists';
+    };
 
-    fields.forEach((field) => {
-        field.addEventListener('input', () => {
-            const errorMessage = field.parentNode.querySelector('.error-message');
-    
-            if (field.classList.contains('error')) {
-                field.classList.remove('error');
-                field.classList.add('no-error');
-                errorMessage.textContent = '';
-            };
+    // Error handling
+    if(!hasError) {
+        users.push({ 
+            name: username, 
+            email: email, 
+            role: role 
         });
-    });
-
+        
+        localStorage.setItem('users', JSON.stringify(users));
+        showData();
+        clearForm();
+    }
 });
 
 // Displaying error
-function showError (element, message) {
-    const errorMessage = element.parentNode.querySelector('.error-message');    
-
+function showError (element) {
     hasError = true;
     element.classList.add('error');
-    element.classList.remove('no-error');
-    errorMessage.style.display = 'block';
-    errorMessage.textContent = message;
+    errorMessage.style.visibility = 'visible';
+    errorMessage.textContent = 'Please fill the required field';
 }
 
 // Input validation
 function validateForm() {
-
     // Name error
-    if (formName.value === '') {
-        showError(formName, 'Name is required');
+    if (usersName.value === '') {
+        showError(usersName);
     };
 
     // Email error
-    if(userEmail.value === '') {
-        showError(userEmail, 'Email is required');
-    } else if(!isValidEmail(userEmail.value)) {
-        showError(userEmail, 'Please enter a valid email');
+    if(usersEmail.value === '') {
+        showError(usersEmail);
+    } else if(!isValidEmail(usersEmail.value)) {
+        showError(usersEmail);
+        errorMessage.textContent = 'Please enter a valid email';
     };
 
     // Select role error
-    if(userRole.selectedIndex === 0) {
-        showError(userRole, 'Role is required')
+    if(usersRole.selectedIndex === 0) {
+        showError(usersRole);
     };
-}
+};
 
 // Email validation
 function isValidEmail(mail) {
@@ -82,149 +77,95 @@ function isValidEmail(mail) {
     return emailRegex.test(String(mail).toLowerCase());
 };
 
-// Adding users to table
-function addUser(names, emails, roles) {
-    const initialButtons =
-    `<ul>
-        <li class="left-btn btn edit"><i class="fa-solid fa-pen-to-square"></i>Edit</li>
-        <li class="right-btn btn delete"><i class="fa-solid fa-trash"></i>Delete</li>
-    </ul>`;
-
-    const editmodeButtons = 
-    `<ul>
-        <li class="left-btn btn update"><i class="fa-solid fa-check"></i>Update</li>
-        <li class="right-btn btn cancel"><i class="fa-solid fa-xmark"></i>Cancel</li>
-    </ul>`;
-
-    const table = document.querySelector('table');
-
-    // Checking for tbody element
-    let tableBody = table.querySelector('tbody');
-    if (!tableBody) {
-        tableBody = document.createElement('tbody');
-        table.appendChild(tableBody);
+// Show local storage data on page
+function showData() {
+    if(localStorage.getItem('users') == null) {
+        users = [];
+    } else{
+        users = JSON.parse(localStorage.getItem('users'));
     };
 
-    const newRow = document.createElement('tr');
+    let html = '';
 
-    // Creating table data for each input and action buttons
-    const nameData = document.createElement('td');
-    nameData.innerHTML = `<input type="text" class="table-data long capitalize" value="${names}" readonly>`;
-
-    const emailData = document.createElement('td');
-    emailData.innerHTML = `<input type="text" class="table-data longer lowercase" value="${emails}" readonly>`;
-
-    const roleData = document.createElement('td');
-    roleData.textContent = roles;
-    roleData.classList.add('capitalize');
-
-    const actionData = document.createElement('td');
-    actionData.classList.add('action');
-    actionData.innerHTML = initialButtons;
-
-    // Adding new table data elements to the new row
-    newRow.appendChild(nameData);
-    newRow.appendChild(emailData);
-    newRow.appendChild(roleData);
-    newRow.appendChild(actionData);
-
-    // Adding new row to table body
-    tableBody.appendChild(newRow);
-
-    // Clearing form after adding new row
-    clearForm();
-
-    let initialName, initialEmail, initialRole;
-
-    // Edit button function
-    const edit = actionData.querySelector('.edit');
-    edit.addEventListener('click', () => {
-        editMode = true;
-
-        // Storing initial state
-        initialName = nameData.querySelector('input').value;
-        initialEmail = emailData.querySelector('input').value;
-        initialRole = roleData.textContent;
-
-        // Changes to table row
-        nameData.querySelector('input').classList.add('edit-mode');
-        nameData.querySelector('input').removeAttribute('readonly');
-
-        emailData.querySelector('input').classList.add('edit-mode');
-        emailData.querySelector('input').removeAttribute('readonly');
-
-        roleData.innerHTML = 
-        `<select class="table-data edit-mode shorter" id="user-role">
-            <option value="user" ${roles === 'user' ? 'selected' : ''}>User</option>
-            <option value="admin" ${roles === 'admin' ? 'selected' : ''}>Admin</option>
-        </select>`;
-
-        actionData.innerHTML = editmodeButtons;
+    users.forEach(function(element, index) {
+        html += '<tr>';
+        html += '<td class="capitalize">' + element.name + '</td>';
+        html += '<td>' + element.email + '</td>';
+        html += '<td class="capitalize">' + element.role + '</td>';
+        html += 
+            '<td class="action-btn"><button onclick="editData(' + index + ')" class="btn left-btn"><i class="fa-solid fa-pen-to-square"></i>Edit</button><button onclick="deleteData(' + index + ')" class="btn right-btn"><i class="fa-solid fa-trash"></i>Delete</button>'
+        html += '</tr>';
     });
 
-    // Delete button function
-    const del = actionData.querySelector('.delete');
-    del.addEventListener('click', () => {
-        // Deleting user data from local storage
-        let currentUsers = JSON.parse(localStorage.getItem('users')) || [];
-        const index = currentUsers.findIndex(user => user.name === names && user.email === emails);
-        users.splice(index, 1);
+    document.querySelector('#table tbody').innerHTML = html;
+};
 
-        localStorage.setItem('users', JSON.stringify(users));
+document.onload = showData();
 
-        // Deleting user from table row
-        const delTR = del.closest('tr');
-        delTR.remove();
-    });
+// Deleting data 
+function deleteData(index) {
+    if(localStorage.getItem('users') == null) {
+        users = [];
+    } else{
+        users = JSON.parse(localStorage.getItem('users'));
+    };
 
-    // Editing mode buttons
-    if(editMode) {
-        // Update button function
-        const update = actionData.querySelector('.update');
-        update.addEventListener('click', () => {
-            const updatedName = nameData.querySelector('input').value;
-            const updatedEmail = emailData.querySelector('input').value;
-            const updatedRole = roleData.querySelector('select').value;
-            // Updating user data in local storage
-            users = users.map(user => {
-                if(user.username === names) {
-                    user.username = updatedName;
-                    user.email = updatedEmail;
-                    user.role = updatedRole;
-                }
-                return user;
-            });
-    
+    users.splice(index, 1);
+    localStorage.setItem('users', JSON.stringify(users));
+    showData();
+};
+
+// Edit data
+function editData(index) {
+    // Displaying update button and hiding add user button
+    addBtn.style.display = 'none';
+    updateBtn.style.display = 'block';
+
+    if(localStorage.getItem('users') == null) {
+        users = [];
+    } else{
+        users = JSON.parse(localStorage.getItem('users'));
+    };
+
+    // Auto populate form
+    usersName.value = users[index].name;
+    usersEmail.value = users[index].email;
+    usersRole.value = users[index].role;
+
+    updateBtn.addEventListener('click', e => {
+        hasError = false;
+        e.preventDefault();
+        validateForm();
+
+        if(!hasError) {
+            // Updating values in local storage
+            users[index].name = usersName.value;
+            users[index].email = usersEmail.value;
+            users[index].role = usersRole.value;
+
             localStorage.setItem('users', JSON.stringify(users));
-            
-            // Changing back to initial state
-            nameData.querySelector('input').classList.remove('edit-mode');
-            nameData.querySelector('input').setAttribute('readonly');
-    
-            emailData.querySelector('input').classList.add('edit-mode');
-            emailData.querySelector('input').setAttribute('readonly');
-    
-            roleData.innerHTML = updatedRole;
-            actionData.innerHTML = initialButtons;
+            showData();
+            clearForm();
 
-            editMode = false;
-        });
-
-        // Cancel button function
-        const cancel = actionData.querySelector('.cancel');
-        cancel.addEventListener('click', () => {
-            nameData.querySelector('input').value = initialName;
-            emailData.querySelector('input').value = initialEmail;
-            roleData.innerHTML = initialRole;
-            actionData.innerHTML = editmodeButtons;
-            editMode = false;
-        });
-    };
+            // Hiding update button and displaying add user button
+            addBtn.style.display = 'block';
+            updateBtn.style.display = 'none';
+        }
+    })
 };
 
 // Clear form
 function clearForm() {
-    formName.value = '';
-    userEmail.value = '';
-    userRole.selectedIndex = 0;
+    usersName.value = '';
+    usersEmail.value = '';
+    usersRole.selectedIndex = 0;
 };
+
+fields.forEach((field) => {
+    field.addEventListener('input', () => {  
+        if (field.classList.contains('error')) {
+            field.classList.remove('error');
+            errorMessage.textContent = '';
+        };
+    });
+});
